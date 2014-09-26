2cc730357180f97f2cf85bd1339a7db9f3e9e8d1
samkeen <sam.keen@nike.com>
2014-09-25 22:09:24 -0700

    now upsert one record per row

--- go.js
+++ go.js
@@ -4,7 +4,7 @@ var mongojs = require('mongojs');
 var fs = require('fs');
 
 var app_path = __dirname;
-var config_path = app_path + "/config.json";
+var config_path = app_path + "/config/config.json";
 var db_config = JSON.parse(fs.readFileSync(config_path, 'utf8'));
 var db_uri_template = _.template("mongodb://<%= username %>:<%= password %>@<%= host %>:<%= port %>/<%= db_name %>");
 var db_uri = db_uri_template({
@@ -15,7 +15,7 @@ var db_uri = db_uri_template({
     db_name: db_config.db_name
 });
 
-var db = mongojs.connect(db_uri, ['demo_test'])
+var db = mongojs(db_uri, ['demo_test', 'audit'])
 
 var options = {
     host: 'api.civicapps.org',
@@ -33,19 +33,41 @@ callback = function(response) {
     //the whole response has been received, so we just print it out here
     response.on('end', function () {
         var resp = JSON.parse(str);
-        db.demo_test.save(resp, function(err, result){
-            if(err) {
-                console.log(err);
-            } else {
-                console.log(result);
-            }
+
+        /**
+         * @TODO add meta data from `resp` to audit table
+         */
+        _.each(resp.results, function(result){
+            console.log("Saving: [" + result.DateAdded + "]" + result.BusinessName);
+
+            /**
+             * perform upsert so we do not duplicate entries
+             * @TODO on insert, $id is returned, record this in audit log
+             */
+            db.demo_test.update({Privacyid: result.Privacyid}, result, {upsert:true}, function(err, save_result){
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(save_result);
+                }
+
+            });
+
 
         });
+        /**
+         * @TODO Add Privacyid for all and $_id's for those created
+         */
+        db.audit.save({date: new Date().toISOString(), records_processed: resp.results.length});
 
     });
 }
 
 try {
+    //// Get the date last checked, add one day
+    //var dateSince = lastDateSince + 1 day;
+    options.path = options.path + "?since=2014-09-01";
+
     http.request(options, callback).end();
 } catch (e) {
     console.log(e);
